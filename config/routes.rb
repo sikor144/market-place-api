62524f2ed085897ab8b96de3aac11811e05951bd
Michał Sikora <sikor1413@gmail.com>
2017-02-26 20:30:56 +0100

    Add sessions controller with create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
   devise_for :users
